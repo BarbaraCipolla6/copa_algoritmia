2f257b63090141d6df99e6c789476e394c2b62d3
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2024-05-09 19:35:10 -0400

    Agrego mas jugadores (10 cada equipo)

--- copa_algoritmia/primer_desafio.py
+++ copa_algoritmia/primer_desafio.py
@@ -1,6 +1,31 @@
 # Se importa el módulo random para usarlo próximamente.
 import random
 
+# Se crean dos listas con cada equipo y dentro se encuentran varias tuplas con los datos solicitados, este tipo de colección se utiliza para evitar la modificación de los mismos.
+equipo_argentino = [("Sofía Toccalino", 2),
+                    ("Agustina Gorzelany", 3),
+                    ("Valentina Raposo", 4)
+                    ("Agostina Alonso", 5),
+                    ("María Forcherio", 6),
+                    ("Agustina Albertarrio", 7),
+                    ("María José Granatto", 10),
+                    ("Delfina Thome", 11),
+                    ("Cristina Cosentino", 13),
+                    ("Clara Barberi", 14)
+                    ]
+
+equipo_australiano = [("Claire Colwill", 1), 
+                    ("Ambrosia Malone", 2), 
+                    ("Brooke Peris", 3), 
+                    ("Amy Lawton", 4), 
+                    ("Grace Young", 5), 
+                    ("Penny Squibb", 6), 
+                    ("Aleisha Power", 7), 
+                    ("Wilson Georgia", 8), 
+                    ("Shanea Tonkin", 9), 
+                    ("Maddy Fitzpatrick", 10)
+                    ]
+
 # Mediante una función que tome los datos de cada equipo como parámetro se implementa el método choice con las dos opciones posibles, según cada opción se toma cada valor individual que contengan las tuplas de cada equipo. Luego del condional, se asignan los valores de los pases y del tiempo de manera aleatoria dentro de listas y finalmente la función devuelve el valor de lo requerido.  
 def pases():
     equipo = random.choice(["Argentina", "Australia"])
@@ -8,35 +33,29 @@ def pases():
         nombre, camiseta = random.choice(equipo_argentino)
     else:
         nombre, camiseta = random.choice(equipo_australiano)
+
     posible_pase = random.choice([0,1])
-    tiempo_partido = random.choice([0,60])
+    tiempo_partido = random.randint(1, 60)
     res_string = f"{equipo};{camiseta};{nombre};{posible_pase};{tiempo_partido}"
+
     return res_string
 
-# Se crean dos listas con cada equipo y dentro se encuentran varias tuplas con los datos solicitados, este tipo de colección se utiliza para evitar la modificación de los mismos.
-equipo_argentino = [("Agustina Gorzelany", 7),
-                    ("María José Granatto", 16),
-                    ("Sofía Toccalino", 11),
-                    ("Luciana Aymar", 15)
-                    ]
-
-equipo_australiano = [("Katrina Powell", 10),
-                    ("Madonna Blyth", 9),
-                    ("Toni Cronk", 8)
-                    ]
-
+# Crear un nuevo archivo e escribir 50.000 eventos aleatorios
 def crearArchivo():
-    # Crear un nuevo archivo
     archivo_pases = open("pases.txt", "w", encoding='utf-8')        
     cont_pases = 0
     eventos = []
 
-    # Activar la funcion para crear un evento de pase aleatorio 50.000 veces
     while cont_pases < 50000:
         evento = pases()
-        eventos.append(f"{evento}\n")
+        if cont_pases < 49999:
+            eventos.append(f"{evento}\n")
+        else:
+            eventos.append(f"{evento}")
         cont_pases += 1
 
     archivo_pases.writelines(eventos)
 
-crearArchivo()
+# Ejecutar programa
+if __name__ == "__main__":
+    crearArchivo()
